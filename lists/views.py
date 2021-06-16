982862e13fcc7372ed881d1ae29a788fc953502d
FazleRabbiFerdaus172 <fazle.ferdaus1416@gmail.com>
2021-06-16 10:43:59 +0600

    all unittest passes

--- lists/views.py
+++ lists/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from .models import Item
 # Create your views here.
@@ -8,7 +8,6 @@ def home_page(request):
     if request.method == 'POST':
         new_item_text = request.POST['item_text']
         Item.objects.create(text=new_item_text)
-    else:
-        new_item_text = ''
+        return redirect('/')
 
-    return render(request, 'home.html', context={'new_item_text': new_item_text})
+    return render(request, 'home.html')
